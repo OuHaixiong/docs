8e3869ba8dc190327d232bbb1f7ff826963ed7ad
ouhaixiong <haixiong.ou@56100.com>
2019-12-03 11:21:06 +0800

    数据库备份恢复

---
+++ shell-postgresql_dump.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+host='xx.xxx.x.xxxx';
+port='5432';
+username='root';
+password='123456';
+databaseName='test_database';
+savePath='/data/databaseBack';
+datetime=`date +%Y%m%d%H%M%S`;
+pgDumpPath='/usr/bin/pg_dump';
+
+export PGPASSWORD=${password};
+`${pgDumpPath} -h ${host} -p ${port} -U ${username} -Ft ${databaseName} > ${savePath}/${databaseName}_${datetime}.tar`;
+echo 'Success!!';
