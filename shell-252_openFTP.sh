35de58630f5afdfc4428fc57fcc8878cd63903d3
ouhaixiong <haixiong.ou@56100.com>
2020-04-15 09:59:07 +0800

    vscode+xdebug

--- shell-252_openFTP.sh
+++ shell-252_openFTP.sh
@@ -4,7 +4,12 @@ dir='/home/ftps/company/';
 password='****';
 homeDir=${dir}${username};
 
+mkdir -p ${homeDir}
+
 useradd ${username} -g www -s /sbin/nologin -d ${homeDir}
+chown -R ${username}.www ${homeDir}
+chmod -R 0777 ${homeDir}
+
 echo ${password}|passwd --stdin ${username}
 
 echo ${username} >> /etc/vsftpd/user_list
@@ -15,3 +20,5 @@ write_enable=YES
 download_enable=YES
 local_umask=002
 EOF
+
+echo 'FTP账号已开通！！！！';
